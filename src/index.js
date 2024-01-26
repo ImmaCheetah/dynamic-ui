2c8e29564531e99eac1017e8a4931228dafd1319
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-01-26 13:39:05 -0500

    Move dropdown logic into separate file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
 import "./style.css";
+import dropdown from "./modules/dropdown"; 
 
-const menuItem = document.querySelectorAll(".dropdown-item");
-const menuHeader = document.querySelector(".dropdown-header");
-
-function swapClasses(target, firstClass, secondClass) {
-  target.forEach((element) => {
-    element.classList.add(firstClass);
-    element.classList.remove(secondClass);
-  });
-}
-
-menuHeader.addEventListener("click", () => {
-  if (menuItem[0].classList.contains("hide")) {
-    swapClasses(menuItem, "visible", "hide");
-  } else {
-    swapClasses(menuItem, "hide", "visible");
-  }
-});
-
-swapClasses(menuItem, "hide");
+dropdown();
---
+++ src/modules/dropdown.js
@@ -0,0 +1,18 @@
+export default function dropdown() {
+    const menuItem = document.querySelectorAll(".dropdown-item");
+    const menuHeader = document.querySelector(".dropdown-header");
+    
+    function toggleHide(target, className) {
+      target.forEach((element) => {
+        element.classList.toggle(className);
+      });
+    }
+    
+    menuHeader.addEventListener("click", () => {
+      toggleHide(menuItem, "hide");
+    });
+    
+    toggleHide(menuItem, "hide");
+}
+
+
