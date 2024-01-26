6766181db098323beb29b1da1f03cfda0ddcdd90
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-01-26 11:52:05 -0500

    Create dropdown, show/hide items when clicked using classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./style.css";
 
+const menuItem = document.querySelectorAll(".dropdown-item");
+const menuHeader = document.querySelector(".dropdown-header");
 
+function swapClasses(target, firstClass, secondClass) {
+  target.forEach((element) => {
+    element.classList.add(firstClass);
+    element.classList.remove(secondClass);
+  });
+}
+
+menuHeader.addEventListener("click", () => {
+  if (menuItem[0].classList.contains("hide")) {
+    swapClasses(menuItem, "visible", "hide");
+  } else {
+    swapClasses(menuItem, "hide", "visible");
+  }
+});
+
+swapClasses(menuItem, "hide");
