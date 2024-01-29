d4ac41b916df38d69f73b1193e908bd68f923264
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-01-29 16:37:24 -0500

    Add css classes in js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ let j = 0;
 const imgArray = [];
 const imageDiv = document.querySelectorAll(".image-div");
 const imageDisplay = document.querySelector(".image-display");
-const carousel = document.querySelector('.carousel-frame');
+const carousel = document.querySelector(".carousel-frame");
 const nextBtn = document.querySelector(".next-btn");
 
 imageDiv.forEach((img) => {
@@ -33,20 +33,33 @@ imageDiv.forEach((img) => {
   imgArray.push(img);
 });
 
-// console.log(i);
 const indexDiv = document.querySelectorAll("[index]");
+console.log(indexDiv.length);
 function nextSlide() {
-  carousel.appendChild(indexDiv[j]);
+  if (j === indexDiv.length - 1) {
+    // Move div outside of display
+    indexDiv[j].classList.add("fade-out");
+    carousel.appendChild(indexDiv[j]);
 
-  j++;
+    indexDiv[j + 1].classList.add(".fade-in");
+    imageDisplay.appendChild(indexDiv[++j]);
+    // indexDiv[j].classList.add('fade-out');
 
-  imageDisplay.appendChild(indexDiv[j]);
+    j = 0;
+  } else {
+    indexDiv[j].classList.add("fade-out");
+    carousel.appendChild(indexDiv[j]);
+
+    indexDiv[j + 1].classList.add(".fade-in");
+    imageDisplay.appendChild(indexDiv[++j]);
+  }
   console.log(j);
-
+  indexDiv[j].classList.add("fade-out");
 }
 
-nextSlide();
-nextSlide();
-nextSlide(imgArray);
-// nextSlide(imgArray);
+nextBtn.addEventListener("click", nextSlide);
 
+// nextSlide();
+// nextSlide();
+// nextSlide();
+// nextSlide(imgArray);
