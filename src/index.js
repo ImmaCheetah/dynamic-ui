8171bee6e818d01dace39040322cccb1d4ae4b83
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-02-01 14:09:11 -0500

    Add styling

--- src/index.js
+++ src/index.js
@@ -9,57 +9,73 @@ let currentSlide = 0;
 let j = 0;
 const imgArray = [];
 const imageDiv = document.querySelectorAll(".image-div");
-const imageDisplay = document.querySelector(".image-display");
-const carousel = document.querySelector(".carousel-frame");
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
 const circleBtn = document.querySelectorAll(".circle");
 
 imageDiv.forEach((img) => {
   imgArray.push(img);
-  img.classList.add('hide');
+  img.classList.add("hide");
 });
 
 function showImage(index) {
-  imageDiv.forEach(img => {
-    img.classList.add('hide')
-  })
-  imageDiv[index].classList.remove('hide');
+  imageDiv.forEach((img) => {
+    img.classList.add("hide");
+  });
+  imageDiv[index].classList.remove("hide");
 }
 
-imgArray[currentSlide].classList.remove('hide');
+function clearSelected() {
+  circleBtn.forEach(btn => {
+    btn.classList.remove('selected');
+  })
+}
+
+imgArray[currentSlide].classList.remove("hide");
+circleBtn[currentSlide].classList.add('selected');
 
 function nextSlide() {
   if (currentSlide === imgArray.length - 1) {
     currentSlide = 0;
     showImage(currentSlide);
+    clearSelected();
+    circleBtn[currentSlide].classList.add('selected');
   } else {
     showImage(++currentSlide);
+    clearSelected();
+    circleBtn[currentSlide].classList.add('selected');
   }
-  console.log(currentSlide);
 }
 
 function previousSlide() {
   if (currentSlide < 1) {
     currentSlide = imgArray.length - 1;
     showImage(currentSlide);
+    clearSelected();
+    circleBtn[currentSlide].classList.add('selected');
   } else {
     showImage(--currentSlide);
+    clearSelected();
+    circleBtn[currentSlide].classList.add('selected');
   }
-  console.log(currentSlide);
 }
 
-
-circleBtn.forEach(btn => {
+circleBtn.forEach((btn) => {
   btn.setAttribute("index", j++);
-   
-  btn.addEventListener('click', () => {
+
+  btn.addEventListener("click", (e) => {
     currentSlide = btn.getAttribute("index");
-    console.log(currentSlide);
+    clearSelected();
+    e.target.classList.add("selected");
     showImage(currentSlide);
-  })
-})
-// showImage(2);
+  });
+});
+
+
+
+// circleBtn.forEach(btn2 => {
+//   btn2.classList.remove('selected');
+// })
 
 nextBtn.addEventListener("click", nextSlide);
 prevBtn.addEventListener("click", previousSlide);
